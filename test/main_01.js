55d3f78ff12ce6843813d050c458863e9418baa0
Harrylj <18200166593@163.com>
2018-03-08 18:42:56 +0800

    2018-03-08-18:42

--- test/main_01.js
+++ test/main_01.js
@@ -5,6 +5,18 @@ var a = 'lijian';
 var b = 'xiaozhi';
 var c = 'shanbao';
 
+process.nextTick(function () {
+    console.log('nextTick callback!');
+});
+console.log('nextTick was set!');
+process.on('exit', function (code) {
+    console.log('about to exit with code: ' + code);
+});
 abc.greet(a);
 abc.hi(b);
 abc.goodbye(c);
+if (typeof(window) === 'undefined') {
+    console.log('node.js');
+} else {
+    console.log('browser');
+}
